2bf11290f0b50bc3336fe588aa36d9a548d6b93f
0xSolidity <88134314+0xSolidity@users.noreply.github.com>
2021-08-26 06:55:34 +0400

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.studio.thegraph.com/query/4199/subgraph/v0.0.1',
+    uri: 'https://thegraph.com/studio/subgraph/subgraph',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -20,7 +20,7 @@ export const healthClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.studio.thegraph.com/query/4199/bscblocks/v0.0.1',
+    uri: 'https://thegraph.com/studio/subgraph/bscblocks',
   }),
   cache: new InMemoryCache(),
 })
